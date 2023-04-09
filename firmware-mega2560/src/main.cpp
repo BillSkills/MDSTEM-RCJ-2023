7940cada162e707301c0a1e62526efae09c7303c
Triple-Edged <usan.siri.trzm@gmail.com>
2023-04-08 20:02:42 -0400

    added SPI peripheral functionality to the mega2560
    to be used to communicate with the other boards
    hopefully it works

--- firmware-mega2560/src/main.cpp
+++ firmware-mega2560/src/main.cpp
@@ -1,61 +1,124 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <SPI.h>
 #include <MeMegaPi.h>
 
 #define SPEED 200
+#define MOVE_DISTANCE 30
 
-MeMegaPiDCMotor motorL(PORT1B);
-MeMegaPiDCMotor motorR(PORT2B);
+volatile bool received;
+volatile byte byteReceived, byteSend;
+
+
+MeMegaPiDCMotor motorLeft(PORT1B);
+MeMegaPiDCMotor motorRight(PORT2B);
 MeUltrasonicSensor ultrasonic(PORT_8);
-MeGyro gyro(PORT6);
+MeUltrasonicSensor ultrasonicLeft(PORT_7);
+MeUltrasonicSensor ultrasonicRight(PORT_6);
+MeGyro gyro(PORT_5);
 
-void move(int distance){
+void move()
+{
   int intialDistance = ultrasonic.distanceCm();
   bool done = false;
-  motorL.run(SPEED);
-  motorR.run(-SPEED);
-  while (!done){
+  motorLeft.run(SPEED);
+  motorRight.run(-SPEED);
+  while (!done)
+  {
     delay(10);
-    done = intialDistance-distance < ultrasonic.distanceCm();
+    done = intialDistance - MOVE_DISTANCE < ultrasonic.distanceCm();
   }
 
   // delay(distance*100);
 
-  motorL.stop();
-  motorR.stop();
+  motorLeft.stop();
+  motorRight.stop();
 }
 
-void turnRight(){
+void turnRight()
+{
   int initialZ = gyro.getAngleZ();
-  motorL.run(50);
-  motorR.run(-50);
-  while(initialZ+90>gyro.getAngleZ()){
+  motorLeft.run(50);
+  motorRight.run(-50);
+  while (initialZ + 90 > gyro.getAngleZ())
+  {
     delay(10);
   }
-  motorL.stop();
-  motorR.stop();
+  motorLeft.stop();
+  motorRight.stop();
 }
 
-void turnLeft(){
+void turnLeft()
+{
   int initialZ = gyro.getAngleZ();
-  motorL.run(-50);
-  motorR.run(50);
-  while(initialZ-90>gyro.getAngleZ()){
+  motorLeft.run(-50);
+  motorRight.run(50);
+  while (initialZ - 90 > gyro.getAngleZ())
+  {
     delay(10);
   }
-  motorL.stop();
-  motorR.stop();
+  motorLeft.stop();
+  motorRight.stop();
 }
 
+// function for setting up the board as an SPI peripheral, so it can talk to the ESPs
+int spi_init()
+{
+  pinMode(MISO, OUTPUT);
+  pinMode(MOSI, INPUT);
 
-void setup(){
-  Serial.begin(9600);
-  move(30);
+  SPCR |= _BV(SPE); // control register stuff i dont understand
+                    // turns on spi in slave mode or something
+  received = false;
+
+  SPI.attachInterrupt();
 }
 
-void loop(){
-  Serial.print("Distance: ");
-  Serial.print(ultrasonic.distanceCm());
-  Serial.println(" cm");
-  delay(100);
+// thing that runs when it receives an spi call?
+ISR(SPI_STC_vect)
+{
+  byteReceived = SPDR; // store the incomming value
+  received = true;     // yep
+}
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+  if (received)
+  {
+    switch (byteReceived)
+    {
+    case 0: // received 0, move 1 space
+      move();
+      break;
+    case 1: // received 1, turn left
+      turnLeft();
+      break;
+    case 2: // received 2, turn right
+      turnRight();
+      break;
+    case 3: // received 3, return measurment from front ultrasonic
+      byteSend = ultrasonic.distanceCm();
+      break;
+    case 4: // received 4, return measurment from front ultrasonic
+      byteSend = ultrasonicLeft.distanceCm();
+      break;
+    case 5: // received 5, return measurment from front ultrasonic
+      byteSend = ultrasonicRight.distanceCm();
+      break;
+
+    default:
+      break;
+    }
+
+    received = false;
+  }
+
+SPDR = byteSend; // sends data back to the master device
+delay(10);
 }
