23da3f90410292d863d6635c0953ffccb0a00d47
Triple-Edged <usan.siri.trzm@gmail.com>
2023-04-08 20:13:57 -0400

    whoops accidentially removed this

---
+++ robot.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+
+/*
+Loop while there are still unexplored nodes
+
+Tile alignment and color check
+
+Wall detection
+
+Add node to maze graph
+
+Victim detection
+
+Rescue kit deployment
+
+Movement to explore next node
+*/
