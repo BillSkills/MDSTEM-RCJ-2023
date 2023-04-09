014f81163a35ea8a517483f61ff2144d077990ac
Triple-Edged <usan.siri.trzm@gmail.com>
2023-04-09 16:42:12 -0400

    added helper functions
    should help with the pathfinding stuff to have these things abstracted away
    no idea if this actually works

--- firmware-esp32/src/main.cpp
+++ firmware-esp32/src/main.cpp
@@ -1,25 +1,70 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <SPI.h>
 
 #define SERVO_PIN         GPIO_NUM_13
 
 Servo servo1;
+
+int rescueKits = 12;
+
+void deployKit(){ // deploy 1 rescue kit
+  if(rescueKits>0){
+    servo1.write(180);
+    delay(100);
+    servo1.write(0);
+    rescueKits --;
+  }
+}
+
+void deployKit(int numKits){  // deploy multiple rescue kits
+  for (int i = 0; i < numKits; i++)
+  {
+    deployKit();
+  }
+  
+}
+
+void move(){
+  SPI.transfer(0);
+}
+
+void turnLeft(){
+  SPI.transfer(1);
+}
+
+void turnRight(){
+  SPI.transfer(2);
+}
+
+int ultrasonic(){
+  return SPI.transfer(3);
+}
+
+int ultrasonicLeft(){
+  return SPI.transfer(4);
+}
+
+int ultrasonicRight(){
+  return SPI.transfer(5);
+}
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(SERVO_PIN, OUTPUT);
-
   servo1.attach(SERVO_PIN);
+  servo1.write(0);
+
+  pinMode(MOSI, OUTPUT);
+  pinMode(MISO, INPUT);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+  digitalWrite(SS, HIGH);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  digitalWrite(LED_BUILTIN, HIGH);
-  servo1.write(180);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  servo1.write(0);
-  delay(1000);
 }
