088d33e8ae3e7af5b35a4c0b7d71730e8ac3b771
BillSkills <billskills01@gmail.com>
2023-04-09 19:00:43 -0400

    modified:   src/main.cpp modified:   ../firmware-mega2560/src/main.cpp modified:   ../src/robot.cpp

--- firmware-esp32/src/main.cpp
+++ firmware-esp32/src/main.cpp
@@ -1,70 +1,63 @@
 #include <Arduino.h>
-#include <Servo.h>
 #include <SPI.h>
+#include <Servo.h>
+#include <algorithm>
+#include <vector>
 
-#define SERVO_PIN         GPIO_NUM_13
+#define SERVO_PIN GPIO_NUM_13
 
 Servo servo1;
 
 int rescueKits = 12;
 
-void deployKit(){ // deploy 1 rescue kit
-  if(rescueKits>0){
+// Deploy 1 rescue kit
+void deployKit() {
+  if (rescueKits > 0) {
     servo1.write(180);
     delay(100);
     servo1.write(0);
-    rescueKits --;
+    rescueKits--;
   }
 }
 
-void deployKit(int numKits){  // deploy multiple rescue kits
-  for (int i = 0; i < numKits; i++)
-  {
+// Deploy multiple rescue kits
+void deployKit(int numKits) {
+  for (int i = 0; i < numKits; i++) {
     deployKit();
   }
-  
 }
 
-void move(){
-  SPI.transfer(0);
-}
+void move() { SPI.transfer(0); }
+void turnLeft() { SPI.transfer(1); }
+void turnRight() { SPI.transfer(2); }
+int ultrasonic() { return SPI.transfer(3); }
+int ultrasonicLeft() { return SPI.transfer(4); }
+int ultrasonicRight() { return SPI.transfer(5); }
 
-void turnLeft(){
-  SPI.transfer(1);
-}
-
-void turnRight(){
-  SPI.transfer(2);
-}
-
-int ultrasonic(){
-  return SPI.transfer(3);
-}
-
-int ultrasonicLeft(){
-  return SPI.transfer(4);
-}
-
-int ultrasonicRight(){
-  return SPI.transfer(5);
-}
-
+// Setup
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(SERVO_PIN, OUTPUT);
   servo1.attach(SERVO_PIN);
   servo1.write(0);
-
   pinMode(MOSI, OUTPUT);
   pinMode(MISO, INPUT);
   SPI.begin();
   SPI.setClockDivider(SPI_CLOCK_DIV8);
   digitalWrite(SS, HIGH);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sensors[3];
+  sensors[0] = ultrasonicLeft();
+  sensors[1] = ultrasonic();
+  sensors[2] = ultrasonicRight();
 
+  for (int i = 0; i < 3; i++) {
+    if (sensors[i] > 10) {
+      sensors[i] = 0;
+    } else {
+      sensors[i] = 1;
+    }
+  }
 }
