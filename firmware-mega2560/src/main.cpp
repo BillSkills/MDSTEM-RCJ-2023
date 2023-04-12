3667c443276a20803c79e21e28a2c910d0411a43
shamwoah <noahskinner5@gmail.com>
2023-04-12 13:33:54 -0400

    turn alg

--- firmware-mega2560/src/main.cpp
+++ firmware-mega2560/src/main.cpp
@@ -79,7 +79,7 @@ bool move() {
   motorRight.run(-MOVE_SPEED);
 
   while (!done) {
-    done = wallDetect();
+    done = ultrasonic.distanceCm() > intialDistance - MOVE_DISTANCE;
     // color.getRGB(&r, &g, &b);
 
     // abort = r < 50 && g < 50 && b < 50;
